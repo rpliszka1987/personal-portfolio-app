65dbe5b4fed34e53144abffcff8acbd36e491d48
Robert Pliszka <robplisz@gmail.com>
2023-09-17 10:51:12 -0400

    Added a header component for the portfolio

---
+++ src/components/Home/Header.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./Header.module.css";
+
+function Header() {
+  return <h1 className={styles.header_style}>Robert Pliszka Portfolio</h1>;
+}
+
+export default Header;
